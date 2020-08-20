f7f2f3daf1933a73da70f5ad88d2fd51a2d39f48
Danroyal001 <danroyaleffiong@gmail.com>
2020-08-20 16:15:40 +0100

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class AppRoot extends StatelessWidget {
       title: 'News and all',
       theme: ThemeData(
         primarySwatch: Colors.red,
+        primaryColor: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
@@ -26,8 +27,15 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("First Rater Media")
-      )
+        title: Text("First Rater Media"),
+        elevation: 30,
+        shadowColor: Colors.grey,
+      ),
+      body: BackDrop(
+        backLayerColor: Colors.red,
+        foreLayerColor: Colors.black,
+        foreLayerWidget: Container(),
+      ),
     );
   }
 }
@@ -35,7 +43,15 @@ class HomePage extends StatelessWidget {
 // BackDrop
 class BackDrop extends StatefulWidget {
   final Key key;
-  BackDrop({this.key}) : super(key: key);
+  final Color backLayerColor;
+  final Color foreLayerColor;
+  final Widget foreLayerWidget;
+  BackDrop(
+      {this.key,
+      @required this.backLayerColor,
+      @required this.foreLayerColor,
+      @required this.foreLayerWidget})
+      : super(key: key);
 
   @override
   _BackDropState createState() => _BackDropState();
@@ -45,13 +61,27 @@ class _BackDropState extends State<BackDrop> {
   @override
   Widget build(BuildContext context) {
     return Stack(children: <Widget>[
-        Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-        ),
-        AnimatedContainer(
-          duration: Duration(milliseconds: 900),
-        ),
-      ]);
+      Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: widget.backLayerColor,
+      ),
+      Align(
+          alignment: Alignment.bottomCenter,
+          child: Card(
+            elevation: 30,
+            child: AnimatedContainer(
+              padding: EdgeInsets.fromLTRB(18, 18, 18, 0),
+              duration: Duration(milliseconds: 900),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height - 56,
+              child: widget.foreLayerWidget,
+              color: widget.foreLayerColor,
+            ),
+          )),
+    ]);
   }
 }
+
+var s =
+    'http://newsapi.org/v2/top-headlines?country=us&apiKey=4d547f2d7ef549c9bb849833e790d744';
